ef3dc0eca153897ac40bd1e8bc3198843143b440
Abigael Valenzuela <87469977+abz-081497@users.noreply.github.com>
2021-10-05 00:06:30 +0800

    Update step.groovy

--- vars/step.groovy
+++ vars/step.groovy
@@ -6,8 +6,8 @@ def buildImage(user){
   myapp = docker.build("${user}/ledger-service:${env.BUILD_ID}", "--build-arg VERSION=${env.BUILD_ID} .")
 }
 
-def buildNpm(){
-  myapp = docker.build("abigael081497/nodejs-test:${env.BUILD_ID}")
+def buildNpm(user){
+  myapp = docker.build("${user}/nodejs-test:${env.BUILD_ID}")
 }
 
 def pushImage(){
