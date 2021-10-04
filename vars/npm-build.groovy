ed182cc9c0ae0f5c8eb7f93021e3a931367135cb
Abigael Valenzuela <87469977+abz-081497@users.noreply.github.com>
2021-10-04 20:09:34 +0800

    Update npm-build.groovy

--- vars/npm-build.groovy
+++ vars/npm-build.groovy
@@ -1,1 +1,30 @@
-
+pipeline {
+    agent any
+    environment {
+        DOCKER_CRED = 'dockerhub'
+    }
+    stages {
+        stage("Checkout code") {
+            steps {
+                checkout scm
+            }
+        }
+        stage("Build image") {
+            steps {
+                script {
+                    myapp = docker.build("darrs08/nodejs-test:${env.BUILD_ID}")
+                }
+            }
+        }
+        stage("Push image") {
+            steps {
+                script {
+                    docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+                            myapp.push("latest")
+                            myapp.push("${env.BUILD_ID}")
+                    }
+                }
+            }
+          }        
+        }
+   }
