9e876d208d31e4e23a09cb34e9257967dbbe212f
Abigael Valenzuela <87469977+abz-081497@users.noreply.github.com>
2021-10-05 10:17:35 +0800

    Create gv.groovy

---
+++ vars/gv.groovy
@@ -0,0 +1,18 @@
+def buildNum(){
+  echo "Build image with tag: ${env.BUILD_ID}"
+}
+
+def buildImage(user){
+  myapp = docker.build("${user}/ledger-service:${env.BUILD_ID}", "--build-arg VERSION=${env.BUILD_ID} .")
+}
+
+def buildNpm(users){
+  myapp = docker.build("${users}/nodejs-test:${env.BUILD_ID}")
+}
+
+def pushImage(){
+  docker.withRegistry('https://registry.hub.docker.com', 'dockerhub'){
+    myapp.push("latest")
+    myapp.push("${env.BUILD_ID}")
+  }
+}
